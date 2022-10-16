7ee77f0073b547473bbdb4d5c7636f0475f01d7d
Keffren <keffrenemail@yahoo.com>
2022-10-16 22:04:42 +0300

    added searchbar

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let hamburger = document.querySelector('.hamburger');
-let header = document.querySelector('header');
-let hidden = [document.querySelector('.attempt'),document.querySelector('.account'),document.querySelector('.basket')];
-let ul = document.querySelector('ul');
-
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('header');
+const hidden = [document.querySelector('.attempt'),document.querySelector('.account'),document.querySelector('.basket')];
+const ul = document.querySelector('ul');
+const search = document.querySelector('section#search');
+const clear = document.querySelector('section#search .searchbar button.clear');
+const searchbar = document.querySelector('section#search .searchbar input');
+let tags = ['tag', 'test', 'lorem', 'ipsum'];
 
 
 hamburger.addEventListener('click',()=>{
@@ -16,6 +19,7 @@ hamburger.addEventListener('click',()=>{
         });
         setTimeout(() => {
             ul.classList.add("switched");                   // move the menu icon to center 
+            search.classList.add("switched");
         }, 200);                                            // wait for the items to hide
 
     } 
@@ -24,14 +28,19 @@ hamburger.addEventListener('click',()=>{
         header.classList.remove('exp');                     // collapse header
 
         ul.classList.remove("switched");
+        search.classList.remove("switched");
 
         setTimeout(() => {
             hidden.forEach(element => {
                 element.classList.remove('hidden');         // showing the logo and the other icons 
             });
         }, 300);                                            // wait for the menu icon to go to center
-
     }
 
 });
+
+clear.addEventListener('click',()=>{
+    searchbar.value = '';
+});
+
 
